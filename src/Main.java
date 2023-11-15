1db695d364aab1b960e68360842634c0d60fbbc4
Sean Aguilar <seanaguilar698@gmail.com>
2023-11-15 21:21:49 +0800

    OOP 1 | Implement user logout functionality and update data files

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,98 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    private static final String USER_DATA_FILE = "user_data.txt";
+    private static final String USER_DATA_PATH = "user_data.txt";
+    private static final String ADMIN_DATA_PATH = "admins.txt";
+    private static final String LOGGED_USERS_FILE_PATH = "loggedUser.txt";
     private static ArrayList<User> users = new ArrayList<>();
+    private static User loggedInUser = null;
+
+    private static void signUp(Scanner scanner) {
+        System.out.print("Enter your full name: ");
+        String fullName = scanner.nextLine();
+
+        System.out.print("Enter your email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Enter your password: ");
+        String password = scanner.nextLine();
+
+        System.out.print("Enter your ID number: ");
+        int idNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        String type = isEmailAdmin(email) ? "Admin" : "Student";
+
+        User newUser = new User(generateUniqueID(), fullName, email, password, idNumber, type, 0);
+        users.add(newUser);
+
+        System.out.println("Sign up successful! Your unique ID is: " + newUser.getUniqueID());
+    }
+
+    private static void login(Scanner scanner) {
+        System.out.print("Enter your email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Enter your password: ");
+        String password = scanner.nextLine();
+
+        for (User user : users) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                loggedInUser = user;
+                System.out.println("Login successful! Welcome " + user.getType() + ", " + user.getFullName());
+                return;
+            }
+        }
+
+        System.out.println("Login failed. Please check your email and password.");
+    }
+
+    private static void logout() {
+        if (loggedInUser != null) {
+            System.out.println("Logging out user: " + loggedInUser.getFullName());
+            users.remove(loggedInUser);
+            removeUserFromLoggedFile(loggedInUser);
+            loggedInUser = null;
+        } else {
+            System.out.println("No user is currently logged in.");
+        }
+    }
+
+    private static boolean isEmailAdmin(String email) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(ADMIN_DATA_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.equals(email)) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private static void removeUserFromLoggedFile(User userToRemove) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(LOGGED_USERS_FILE_PATH));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(LOGGED_USERS_FILE_PATH, false))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] userData = line.split(",");
+                int uniqueID = Integer.parseInt(userData[0]);
+
+                if (uniqueID != userToRemove.getUniqueID()) {
+                    writer.write(line);
+                    writer.newLine();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private static void loadUsersFromFile() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(USER_DATA_FILE))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(USER_DATA_PATH))) {
             String line;
 
             while ((line = reader.readLine()) != null) {
@@ -28,48 +115,8 @@ public class Main {
         }
     }
 
-    private static void signUp(Scanner scanner) {
-        System.out.print("Enter your full name: ");
-        String fullName = scanner.nextLine();
-
-        System.out.print("Enter your email: ");
-        String email = scanner.nextLine();
-
-        System.out.print("Enter your password: ");
-        String password = scanner.nextLine();
-
-        System.out.print("Enter your ID number: ");
-        int idNumber = scanner.nextInt();
-
-        System.out.print("Enter your user type (Student/Admin): ");
-        String type = scanner.next();
-
-        // Assume cesPoints is 0 during signup
-        User newUser = new User(generateUniqueID(), fullName, email, password, idNumber, type, 0);
-        users.add(newUser);
-
-        System.out.println("Sign up successful! Your unique ID is: " + newUser.getUniqueID());
-    }
-
-    private static void login(Scanner scanner) {
-        System.out.print("Enter your email: ");
-        String email = scanner.nextLine();
-
-        System.out.print("Enter your password: ");
-        String password = scanner.nextLine();
-
-        for (User user : users) {
-            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
-                System.out.println("Login successful! Welcome, " + user.getFullName());
-                return;
-            }
-        }
-
-        System.out.println("Login failed. Please check your email and password.");
-    }
-
     private static void saveUsersToFile() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(USER_DATA_FILE))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(USER_DATA_PATH))) {
             for (User user : users) {
                 String userData = String.format("%d,%s,%s,%s,%d,%s,%d",
                         user.getUniqueID(), user.getFullName(), user.getEmail(),
@@ -79,6 +126,20 @@ public class Main {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void saveLoggedUsersToFile() {
+        if (loggedInUser != null) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOGGED_USERS_FILE_PATH, true))) {
+                String userData = String.format("%d,%s,%s,%s,%d,%s,%d",
+                        loggedInUser.getUniqueID(), loggedInUser.getFullName(), loggedInUser.getEmail(),
+                        loggedInUser.getPassword(), loggedInUser.getIDNumber(), loggedInUser.getType(), loggedInUser.getCESPoints());
+                writer.write(userData);
+                writer.newLine();
+            } catch (IOException e) {
+                e.printStackTrace(); // Handle the exception properly in a real-world scenario
+            }
         }
     }
 
@@ -94,7 +155,8 @@ public class Main {
         while (true) {
             System.out.println("1. Sign Up");
             System.out.println("2. Log In");
-            System.out.println("3. Exit");
+            System.out.println("3. Log Out");
+            System.out.println("4. Exit");
             System.out.print("Choose an option: ");
 
             int choice = scanner.nextInt();
@@ -103,12 +165,16 @@ public class Main {
             switch (choice) {
                 case 1:
                     signUp(scanner);
+                    saveUsersToFile();
                     break;
                 case 2:
                     login(scanner);
+                    saveLoggedUsersToFile();
                     break;
                 case 3:
-                    saveUsersToFile();
+                    logout();
+                    break;
+                case 4:
                     System.out.println("Exiting program. Goodbye!");
                     System.exit(0);
                 default:
